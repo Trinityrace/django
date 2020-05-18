8900ec590d3b45975aece590ef691e6d09220b81
Trinity John <trinityrtime@gmail.com>
2020-05-18 12:28:47 +0300

    urls.py route fix./

--- heyapp/urls.py
+++ heyapp/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^helloapp/', include('helloapp.site.urls')),
+    url(r'^helloapp/', include('helloapp.urls')),
 ]
